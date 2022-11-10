4b0938007ee969e89268a7f1272ecdb0e8afbbaa
Louis Dupraz <114499642+LouisDupraz@users.noreply.github.com>
2022-11-11 00:04:52 +0100

    Add to path option and debug logs

--- setup.sh
+++ setup.sh
@@ -15,3 +15,17 @@ printf "Autorun folder and files created\n"
 (crontab -l 2>/dev/null || true; echo "@reboot sh /home/$USER/.autorun/maintain_active.sh") | crontab -
 
 printf "Crontab - Autorun entry created\n"
+
+while [ true ]; do
+    printf "\n  Add the program to path to be able to run the script from anywhere ?\n"
+    read -n 1 k
+    if [ $k == "y" ]; then
+        echo "PATH=$PATH:"/home/$USER/.autorun"
+        break
+    elif [ $k == "n" ]; then
+        break
+    fi
+done
+
+printf "\nSetup complete, script will autorun on boot from now on\n"
+
