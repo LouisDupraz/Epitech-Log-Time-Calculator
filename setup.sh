447a6a6186944e8f2562e1556f6908fd7af40633
Louis Dupraz <114499642+LouisDupraz@users.noreply.github.com>
2022-11-10 23:44:33 +0100

    Debug prints added

--- setup.sh
+++ setup.sh
@@ -10,4 +10,8 @@ if [ "$(ls -a /home/$USER/.autorun/)" == "" ]; then
     cp maintain_active.sh "/home/$USER/.autorun/"
 fi
 
+printf "Autorun folder and files created\n"
+
 (crontab -l 2>/dev/null || true; echo "@reboot sh /home/$USER/.autorun/maintain_active.sh") | crontab -
+
+printf "Crontab - Autorun entry created\n"
