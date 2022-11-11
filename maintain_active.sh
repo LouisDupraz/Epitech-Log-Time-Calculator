e3ee7c4019ffbb13a8d7a6a3c7259ae5ebc002b2
Louis Dupraz <louis.dupraz@epitech.eu>
2022-11-11 12:18:28 +0100

    Debug logs

--- maintain_active.sh
+++ maintain_active.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+USER=$(id -nu 1000)
+
+echo "Init at $(date)" >> "/home/$USER/.autorun/nw_init.log"
+
 sleep 90
+
+echo "Started detection at $(date)" >> "/home/$USER/.autorun/nw_init.log"
 
 SSID=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2)
 
+echo $SSID >> "/home/$USER/.autorun/nw_init.log"
+
 if [ $SSID == "IONIS" ]; then
-    echo "Started activity on $(date)" >> "/home/$(id -nu 1000)/.autorun/nw_activity.log"
+    echo "Started activity on $(date)" >> "/home/$USER/.autorun/nw_activity.log"
     tmux new -d 'ping 8.8.8.8'
 
 fi
