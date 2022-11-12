b304fb7a7c9bfd7001f614fc3ca3a8c405c00a1a
Louis Dupraz <louis.dupraz@epitech.eu>
2022-11-12 18:52:30 +0100

    now downloading the last version of arch

--- loop.sh
+++ loop.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+ARCHVERSION=$(curl https://archlinux.org/download/ | grep "<li><strong>Current Release:</strong> " | cut -d\> -f4 | cut -d\< -f1 | tr -d ' ')
+
 while [ true ]; do
-    wget http://archlinux.mirrors.ovh.net/archlinux/iso/2022.11.01/archlinux-x86_64.iso
+    wget http://archlinux.mirrors.ovh.net/archlinux/iso/$ARCHVERSION/archlinux-x86_64.iso
     rm -rf *.iso
 done
