d665fc53c3d476753f97e0250e641e17ef963ea9
Louis Dupraz <louis.dupraz@epitech.eu>
2022-11-14 20:55:54 +0100

    Typo

--- maintain_active.sh
+++ maintain_active.sh
@@ -5,7 +5,7 @@ sleep 90
 #echo "Started dectection on $(date)" >> /home/LouisD/scripts/nw_init.log
 SSID=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2)
 
-"echo "SSID = $SSID" >> /home/LouisD/scripts/nw_init.log
+#echo "SSID = $SSID" >> /home/LouisD/scripts/nw_init.log
 if [ $SSID == "freebox_moka" ]; then
     echo "Started activity on $(date)" >> "/home/LouisD/scripts/nw_activity.log";
     sh /home/LouisD/scripts/stream.sh
