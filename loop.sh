b27771f4d3797e1fb9638b3af1ed7a4edfbaf1d2
Louis Dupraz <114499642+LouisDupraz@users.noreply.github.com>
2022-11-14 15:08:10 +0100

    Delete loop.sh

--- loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ARCHVERSION=$(curl https://archlinux.org/download/ | grep "<li><strong>Current Release:</strong> " | cut -d\> -f4 | cut -d\< -f1 | tr -d ' ')
-
-while [ true ]; do
-    wget http://archlinux.mirrors.ovh.net/archlinux/iso/$ARCHVERSION/archlinux-x86_64.iso
-    rm -rf *.iso
-done
