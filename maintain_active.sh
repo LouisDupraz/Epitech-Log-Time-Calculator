a3a7291c5c6f4181ac0572d2567f1ad5a3d99694
Louis Dupraz <louis.dupraz@epitech.eu>
2022-11-14 22:04:47 +0100

    Added VLC automute

--- maintain_active.sh
+++ maintain_active.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-#echo "Init on $(date)" >> /home/LouisD/scripts/nw_init.log
+
 sleep 90
 
-#echo "Started dectection on $(date)" >> /home/LouisD/scripts/nw_init.log
 SSID=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2)
 
-#echo "SSID = $SSID" >> /home/LouisD/scripts/nw_init.log
 if [ $SSID == "IONIS" ]; then
     echo "Started activity on $(date)" >> "/home/LouisD/scripts/nw_activity.log";
     sh /home/LouisD/scripts/stream.sh
--- stream.sh
+++ stream.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
-#echo "stream.sh started" >> "/home/LouisD/scripts/nw_activity.log";
-tmux new -d "cvlc --no-audio 'https://www.youtube.com/watch?v=jfKfPfyJRdk' -V dummy"
+tmux new -d "cvlc 'https://www.youtube.com/watch?v=jfKfPfyJRdk' -V dummy"
+sleep 5
+app_name="VLC media player (LibVLC $(pactl list sink-inputs | grep 'application.name = ' | cut -d\( -f2 | cut -d\) -f1 | cut -dC -f2 | tr -d ' '))"
+
+current_sink_num=''
+sink_num_check=''
+app_name_check=''
+
+pactl list sink-inputs |while read line; do \
+    sink_num_check=$(echo "$line" |sed -rn 's/^Sink Input #(.*)/\1/p')
+    if [ "$sink_num_check" != "" ]; then
+        current_sink_num="$sink_num_check"
+    else
+        app_name_check=$(echo "$line" \
+            |sed -rn 's/application.name = "([^"]*)"/\1/p')
+        if [ "$app_name_check" = "$app_name" ]; then
+            pactl set-sink-input-mute "$current_sink_num" toggle
+        fi
+    fi
+done
+
 exit 0
