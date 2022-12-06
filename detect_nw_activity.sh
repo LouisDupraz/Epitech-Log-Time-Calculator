c8243b443dd58e6e10e34c33666e8fad5f674d5a
Louis Dupraz <louis.dupraz@epitech.eu>
2022-12-06 13:11:13 +0100

    added log time at shutdown/reboot

--- detect_nw_activity.sh
+++ detect_nw_activity.sh
@@ -2,6 +2,7 @@
 
 sleep 10
 
+USER=$(id -nu 1000)
 SSID=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2)
 NWACTIVE=0
 
@@ -16,19 +17,20 @@ while [ true ]; do
         if [ $NWACTIVE -eq 0 ]; then
             echo "Started activity on $(date)" >> "/home/logtime.log";
             NWACTIVE=1
-            TIMEINIT=$(date +%s)
+            echo $(date +%s) > "/home/$USER/.autorun/inittime"
         fi
     fi
     if [ "$SSID" != "IONIS" ]; then
         if [ $NWACTIVE -eq 1 ]; then
             echo "End on activity on $(date)" >> "/home/logtime.log"
             NWACTIVE=0
+            TIMEINIT=$(cat "/home/$USER/.autorun/inittime" | bc)
             TIMEEND=$(date +%s)
             DELTATIME=$((TIMEEND - TIMEINIT))
             H=$(echo "scale=0; $DELTATIME/3600 % 24" | bc)
             M=$(echo "scale=0; $DELTATIME/60 % 60" | bc)
             S=$(echo "scale=0; $DELTATIME % 60" | bc)
-            echo "Device was active for $H h, $M m, $S s" >> "/home/logtime.log"
+            echo "Device was active for $H h, $M m, $S s ($DELTATIME s)" >> "/home/logtime.log"
         fi
     fi
 done
@@ -36,12 +38,13 @@ done
 if [ $NWACTIVE -eq 1 ]; then
     echo "End on activity on $(date)" >> "/home/logtime.log"
     NWACTIVE=0
+    TIMEINIT=$(cat "/home/$USER/.autorun/inittime" | bc)
     TIMEEND=$(date +%s)
     DELTATIME=$((TIMEEND - TIMEINIT))
     H=$(echo "scale=0; $DELTATIME/3600 % 24" | bc)
     M=$(echo "scale=0; $DELTATIME/60 % 60" | bc)
     S=$(echo "scale=0; $DELTATIME % 60")
-    echo "Device was active for $H h, $M m, $S s" >> "/home/logtime.log"
+    echo "Device was active for $H h, $M m, $S s ($DELTATIME s)" >> "/home/logtime.log"
 fi
 
 
